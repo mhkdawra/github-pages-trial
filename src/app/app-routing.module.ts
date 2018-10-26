2d7ecbff44f351501af9995fd995057967da813a
Mehak <mehak@MAC-41.local>
2018-10-26 15:11:52 +0530

    Added suggested changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about/:id', component: AboutComponent }
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
